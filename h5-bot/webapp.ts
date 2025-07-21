a54ad58229643f90f158ef66411fd30c9a781331
ningyongheng <ningyongheng@jeejio.com>
2025-07-21 15:11:50 +0800

    feat: 配置当前的bot运算和h5小应用联动

--- h5-bot/webapp.ts
+++ h5-bot/webapp.ts
@@ -14,18 +14,26 @@ import {
 } from 'grammy'
 
 const bot = new Bot('7996507522:AAEtMBRvfLZbTQJxkyWmSuIJRoBtA0Tot2I')
-const WEB_APP_URL = 'https://t.me/maopaoBot/jeejiohome'
+const WEB_APP_URL =
+    'https://storage.jeejio.com/jeejio-debug/telegrambot/index.html'
 
 // ——给所有聊天（Default）都设置 Web App 按钮：
 await bot.api.setChatMenuButton({
     menu_button: {
         type: 'web_app',
-        text: '🚀 打开小程序',
+        text: '🚀 打开小程序22',
         web_app: { url: WEB_APP_URL },
     },
-    
 })
 
+bot.on('message', async ctx => {
+    // 处理消息事件
+    if (ctx.message.text === '打开小程序') {
+        // 发送 Web App 按钮
+        await ctx.reply('点击下面的按钮打开小程序')
+    }
+    console.log('Received message:', ctx.message)
+})
 bot.catch(err => console.error(err))
 
 // 启动 Bot（长轮询）
